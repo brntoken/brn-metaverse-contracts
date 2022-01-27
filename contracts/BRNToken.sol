2dc7182ff8b16e4965fa516560c5039cd58fb068
Dickens-Odera <dickensodera9@gmail.com>
2022-01-27 17:09:11 +0300

    feat: Added basic token functinalities

--- contracts/BRNToken.sol
+++ contracts/BRNToken.sol
@@ -1,7 +1,66 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
+import './interfaces/IBEP2E.sol';
 
-contract BRNToken {
+contract BRNToken is IBEP2E {
+
+  string public _name;
+  string public _symbol;
+  uint public _totalSuplly;
+  uint public _decimals;
+
+  mapping(address => uint) public balances;
+  mapping(address => mapping(address => uint)) public allowances;
+
   constructor() public {
+    _name = "BRN Token";
+    _symbol = "BRN";
+    _decimals = 18;
+    _totalSuplly = 1000000000 * 10 ** _decimals;
   }
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value)
+
+  function name() public view returns(string){
+    return _name;
+  }
+
+  function symbol() public view returns(string){
+    return _symbol;
+  }
+
+  function decimals() pulic view returns(uint){
+    return _decimals;
+  }
+
+  function totalSupply() public view returns(uint){
+    return _totalSuplly;
+  }
+
+  function balanceOf(address account) public view returns (uint256){
+
+  }
+
+  function transfer(address recipient, uint256 amount) public returns (bool){
+
+  }
+
+  function transferFrom(address sender, address recipient, uint256 amount) public returns (bool){
+
+  }
+
+  function allowance(address _owner, address spender) public view returns (uint256){
+
+  }
+
+  function approve(address spender, uint256 amount) public returns (bool){
+
+  }
+
+
+  function getOwner() public view returns (address){
+
+  }
+
 }
