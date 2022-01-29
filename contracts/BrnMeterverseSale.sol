28e878af9d8cf1c54ecf824096d65817b5ad21a5
Dickens-Odera <dickensodera9@gmail.com>
2022-01-30 01:48:06 +0300

    feat: Initialize presale contract

--- contracts/BrnMeterverseSale.sol
+++ contracts/BrnMeterverseSale.sol
@@ -2,8 +2,18 @@
 pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC20/utils/TokenTimeLock.sol';
 
-
+/**
+* @title BrnMeterversePreSale
+* @dev A contract that manages the BRN Meterverse Presale logic
+*/
 contract BrnMeterversePreSale {
+
+  //presale phases
+  enum ICOPresalePhase { Phase1, Phase2, Phase3}
+
+  //set the default ICO phase
+  ICOPresalePhase public icoPhase = ICOPresalePhase.Phase1;
+
   constructor() public {
   }
 }
