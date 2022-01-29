f348c75f3b154a24603e778c985f4e0bae8f9a70
Dickens-Odera <dickensodera9@gmail.com>
2022-01-29 07:12:10 +0300

    feat: Passing test on BRN transfers and new compiled contract artifacts

--- test/brn_meterverse_test.js
+++ test/brn_meterverse_test.js
@@ -1,4 +1,5 @@
 const BrnMeterverse = artifacts.require('BrnMeterverse');
+const web3 = require('web3');
 
 contract('BrnMeterverse',(accounts) =>{
     let brnMeterverse;
@@ -15,18 +16,18 @@ contract('BrnMeterverse',(accounts) =>{
     });
 
     describe("BRN Meterverse Transfers", () => {
-        it("can enable token transfers", async() => {
+        it("can enable meterverse token transfers", async() => {
             const amount = 10;
-            const initialOwnerBalance = await btnMeterverse.balanceOf(owner);
-            console.log("Initial Owner Balance",initialOwnerBalance);
+            const initialOwnerBalance = await brnMeterverse.balanceOf(owner);
+            console.log("Initial Owner Balance",initialOwnerBalance.toNumber());
             const result = await brnMeterverse.transfer(alice, amount, { from: owner });
 
             const aliceBalance = await brnMeterverse.balanceOf(alice);
-            const newOwnerBalance = await btnMeterverse.balanceOf(owner);
+            const newOwnerBalance = await brnMeterverse.balanceOf(owner);
 
             assert(result.receipt.status, true);
             assert.equal(aliceBalance, 10,"Meterverse is deposited successfully into other wallet address");
-            assert(initialOwnerBalance < newOwnerBalance,"Owner balance reduces upon sending some metervers to another address");
+            assert(initialOwnerBalance < newOwnerBalance,"Owner balance reduces upon sending some meterverse tokens to another address");
         });
     });
 
