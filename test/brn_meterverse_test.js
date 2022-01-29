41b84115cc85e4dfd5fba6751f73450796fa70f1
Dickens-Odera <dickensodera9@gmail.com>
2022-01-29 09:08:26 +0300

    feat: succesfully test that BRN totl supply is increased upon minting

--- test/brn_meterverse_test.js
+++ test/brn_meterverse_test.js
@@ -16,7 +16,7 @@ contract('BrnMeterverse',(accounts) =>{
     });
 
     describe("BRN Meterverse Transfers", () => {
-        it("can enable meterverse token transfers", async() => {
+        it("can successfuly enable BRN transfers from one address to another", async() => {
             const amount = 10;
             const initialOwnerBalance = await brnMeterverse.balanceOf(owner);
             console.log("Initial Owner Balance",initialOwnerBalance.toNumber());
@@ -33,7 +33,7 @@ contract('BrnMeterverse',(accounts) =>{
             assert(result.logs[0].args.value, 10,"BRN amount transfered captured correctly");
         });
 
-        it("can enable BRN holder to approve allowance to another address", async() => {
+        it("can successfuly enable a BRN holder to approve allowance to another address", async() => {
             const allowanceAmount = 1000;
 
             const currentOwnerAllowanceToAlice = await brnMeterverse.allowance(owner, alice);
@@ -49,7 +49,7 @@ contract('BrnMeterverse',(accounts) =>{
             assert(result.logs[0].args.value, allowanceAmount);
         });
 
-        it("can enable a BRN holder to increase the allowance issued to another address to be spent on their behalf", async() =>{
+        it("can successfuly enable a BRN holder to increase the allowance issued to another address to be spent on their behalf", async() =>{
             const allowanceToBeAdded = 2000;
             const currentOwnerAllowanceToAlice = await brnMeterverse.allowance(owner, alice);
 
@@ -66,11 +66,10 @@ contract('BrnMeterverse',(accounts) =>{
             assert(result.logs[0].args.value, allowanceToBeAdded);
         });
 
-        it("can enable a BRN holder to descrease the allowance issued to another address to be spent on their behalf", async () => {
+        it("can successfuly enable a BRN holder to descrease the allowance issued to another address to be spent on their behalf", async () => {
             const allowanceToBeReduced = 2000;
             const currentOwnerAllowanceToAlice = await brnMeterverse.allowance(owner, alice);
 
-            console.log("Current Allowance", currentOwnerAllowanceToAlice.toNumber());
             const result = await brnMeterverse.decreaseAllowance(alice, allowanceToBeReduced, { from: owner });
             const newAllowance = currentOwnerAllowanceToAlice.toNumber() - allowanceToBeReduced;
 
@@ -78,10 +77,30 @@ contract('BrnMeterverse',(accounts) =>{
 
             assert(result.receipt.status, true);
             assert.equal(newAllowance, 1000, "BRN allowance to spender successfully deacreased by BRN holder");
+            assert.equal(allowanceBalance.toNumber(), 1000);
             //test Approval event
             assert(result.logs[0].args.owner, owner, "Approving address is captured correctly");
             assert(result.logs[0].args.spender, alice, "Spender Address is captured correcty");
             assert(result.logs[0].args.value, allowanceToBeReduced);
+        });
+
+        it("can successfuly enable the BRN Meterverse contract owner to mint more BRN and increase BRN suppply", async() => {
+            const newSupplyToBeAdded = 1000000000;
+            const currentBRNTotalSupply = await brnMeterverse.totalSupply();
+            const currentOwnerBRNBalance = await brnMeterverse.balanceOf(owner);
+            const result = await brnMeterverse.mint(newSupplyToBeAdded, { from: owner} );
+            
+            const newTotalSupply = await brnMeterverse.totalSupply();
+            const newOwnerBalance = await brnMeterverse.balanceOf(owner);
+
+            const totalOwnerBalanceAfterMinting = newSupplyToBeAdded + currentOwnerBRNBalance.toNumber();
+            
+            const newSupply = newSupplyToBeAdded + currentBRNTotalSupply.toNumber();
+
+
+            assert(result.receipt.status, true);
+            assert.equal(newSupply, newTotalSupply,"BRN total Supply increased successfully after mint is triggered by contract owner");
+            assert.equal(totalOwnerBalanceAfterMinting, newOwnerBalance.toNumber(),"BRN owner balance is increased after minting");
         });
     });
 
