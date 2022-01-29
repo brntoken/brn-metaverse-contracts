b3ce2140e60720084ee70811c2eb958913dad716
Dickens-Odera <dickensodera9@gmail.com>
2022-01-29 05:24:14 +0300

    feat: Added BRN pausable functionalitities

--- contracts/BrnMeterverse.sol
+++ contracts/BrnMeterverse.sol
@@ -5,30 +5,64 @@ pragma abicoder v2;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-import '@openzeppelin/contracts/security/Pausable.sol';
 import './interfaces/IBEP2E.sol';
 
 //TODO - Inherit from ReentrancyGuard
-contract BrnMeterverse is Ownable, IBEP2E , Pausable {
+contract BrnMeterverse is Ownable, IBEP2E {
   using SafeMath for uint256;
 
   string public _name; //token name
   string public _symbol; //token symbol 
   uint private _totalSupply; //total supply
-  uint public _decimals; //the total number of decimal represenations
+  uint8 public _decimals; //the total number of decimal represenations
+  bool private _paused;
 
   mapping(address => uint) private balances; //how token much does this address have
   mapping(address => mapping(address => uint)) private allowances; //the amount approved by the owner to be spent on their behalf
+  event Unpaused(address account); // Emitted when the pause is triggered by `account`.
+  event Paused(address account); //Emitted when the pause is lifted by `account`.
 
   constructor() public {
     _name = "Brn Metaverse";
     _symbol = "BRN";
     _decimals = 18;
     _totalSupply = 1000000000;
+    _paused = false;
     balances[msg.sender] = balances[msg.sender].add(_totalSupply);
     emit Transfer(address(0), msg.sender, _totalSupply);
   }
 
+  /**
+  * @dev Modifier to make a function callable only when the contract is not paused.
+  *
+  * Requirements:
+  *
+  * - The contract must not be paused.
+  */
+  modifier whenNotPaused() {
+    require(!paused(), "Pausable: paused");
+     _;
+  }
+
+  /**
+  * @dev Modifier to make a function callable only when the contract is paused.
+  *
+  * Requirements:
+  *
+  * - The contract must be paused.
+  */
+  modifier whenPaused() {
+    require(paused(), "Pausable: not paused");
+    _;
+  }
+
+  /**
+  * @dev Returns true if the contract is paused, and false otherwise.
+  */
+  function paused() public view returns (bool) {
+    return _paused;
+  }
+   
   /**
   * @notice token name
   * @return string token name
@@ -72,11 +106,11 @@ contract BrnMeterverse is Ownable, IBEP2E , Pausable {
   /**
   * @notice how much token balance does this address have
   * @dev the account should not be the zero address , address(0)
-  * @param account account the address to which we want to determine their token balance
+  * @param _account account the address to which we want to determine their token balance
   * @return uint the total balance of the specied address
   */
-  function balanceOf(address account) public view override returns (uint256){
-    return balances[account];
+  function balanceOf(address _account) public view override returns (uint256){
+    return balances[_account];
   }
 
   /**
@@ -106,7 +140,7 @@ contract BrnMeterverse is Ownable, IBEP2E , Pausable {
   */
   function transferFrom(address _sender, address _recipient, uint _amount) public override whenNotPaused returns (bool) {
     _transfer(_sender, _recipient, _amount);
-    _approve(_sender, _msgSender(), allowances[_sender][msg.sender].sub(_amount, "BEP2E: transfer amount exceeds allowance"));
+    _approve(_sender, msg.sender, allowances[_sender][msg.sender].sub(_amount, "BEP2E: transfer amount exceeds allowance"));
     return true;
   }
 
@@ -186,25 +220,57 @@ contract BrnMeterverse is Ownable, IBEP2E , Pausable {
   }
 
   /**
+  * @dev Triggers stopped state.
+  *
+  * Requirements:
+  *
+  * - The contract must not be paused.
+  */
+  function pause() internal onlyOwner whenNotPaused {
+    _paused = true;
+    emit Paused(msg.sender);
+  }
+
+  /**
+  * @dev Returns to normal state.
+  *
+  * Requirements:
+  *
+  * - The contract must be paused.
+  */
+  function unpause() internal onlyOwner whenPaused {
+    _paused = false;
+    emit Unpaused(msg.sender);
+  }
+
+  
+  /**
   * -- INTERNAL FUNCTIONS -- 
   */
 
   /**
-   * @dev Moves tokens amount from sender to recipient.
-   * @dev _sender cannot be the zero address.
-   * @dev recipient cannot be the zero address.
-   * @dev sender must have a balance of at least amount
-   * @param _sender address thes account sending the tokens amount
-   * @param _recipient address the account receiving the tokens
-   * @param _amount uint the token amount to be sent
-   */
-  function _transfer(address _sender, address _recipient, uint _amount) internal {
+  * @dev Moves tokens amount from sender to recipient.
+  * @dev _sender cannot be the zero address.
+  * @dev recipient cannot be the zero address.
+  * @dev sender must have a balance of at least amount
+  * @param _sender address thes account sending the tokens amount
+  * @param _recipient address the account receiving the tokens
+  * @param _amount uint the token amount to be sent
+  */
+  function _transfer(address _sender, address _recipient, uint _amount) internal virtual {
     require(_sender != address(0), "BEP2E: transfer from the zero address");
     require(_recipient != address(0), "BEP2E: transfer to the zero address");
+
+    _beforeTokenTransfer(_sender, _recipient, _amount);
+
+    uint senderBalance = balances[_sender];
+
+    require(senderBalance >= _amount, "BEP2E: transfer amount exceeds balance");
 
     balances[_sender] = balances[_sender].sub(_amount, "BEP2E: transfer amount exceeds balance");
     balances[_recipient] = balances[_recipient].add(_amount);
     emit Transfer(_sender, _recipient, _amount);
+    _afterTokenTransfer(_sender, _recipient, _amount);
   }
 
   /** @dev Creates amount tokens and assigns them to account, increasing
@@ -213,28 +279,15 @@ contract BrnMeterverse is Ownable, IBEP2E , Pausable {
    * @param _account address 
    * @param _amount uint 
    */
-  function _mint(address _account, uint _amount) internal {
+  function _mint(address _account, uint _amount) internal virtual {
     require(_account != address(0), "BEP2E: mint to the zero address");
+
+    _beforeTokenTransfer(address(0), _account, _amount);
 
     _totalSupply = _totalSupply.add(_amount);
     balances[_account] = balances[_account].add(_amount);
     emit Transfer(address(0), _account, _amount);
-  }
-
-  /**
-   * @dev Sets amount as the allowance of spender over the owner`s tokens.
-   * @dev owner cannot be the zero address.
-   * @dev spender cannot be the zero address.
-   * @param _owner address
-   * @param _spender address
-   * @param _amount uint  
-   */
-  function _approve(address _owner, address _spender, uint _amount) internal {
-    require(_owner != address(0), "BEP2E: approve from the zero address");
-    require(_spender != address(0), "BEP2E: approve to the zero address");
-
-    allowances[_owner][_spender] = _amount;
-    emit Approval(_owner, _spender, _amount);
+    _afterTokenTransfer(address(0), _account, _amount);
   }
 
   /**
@@ -245,12 +298,31 @@ contract BrnMeterverse is Ownable, IBEP2E , Pausable {
    * @param _account address
    * @param _amount uint 
    */
-  function _burn(address _account, uint _amount) internal {
+  function _burn(address _account, uint _amount) internal virtual {
     require(_account != address(0), "BEP2E: burn from the zero address");
+
+    _beforeTokenTransfer(_account, address(0), _amount);
 
     balances[_account] = balances[_account].sub(_amount, "BEP2E: burn amount exceeds balance");
     _totalSupply = _totalSupply.sub(_amount);
     emit Transfer(_account, address(0), _amount);
+    _afterTokenTransfer(_account, address(0), _amount);
+  }
+
+  /**
+   * @dev Sets amount as the allowance of spender over the owner`s tokens.
+   * @dev owner cannot be the zero address.
+   * @dev spender cannot be the zero address.
+   * @param _owner address
+   * @param _spender address
+   * @param _amount uint  
+   */
+  function _approve(address _owner, address _spender, uint _amount) internal virtual {
+    require(_owner != address(0), "BEP2E: approve from the zero address");
+    require(_spender != address(0), "BEP2E: approve to the zero address");
+
+    allowances[_owner][_spender] = _amount;
+    emit Approval(_owner, _spender, _amount);
   }
 
   /**
@@ -259,8 +331,49 @@ contract BrnMeterverse is Ownable, IBEP2E , Pausable {
    * @param _account address
    * @param _amount uint 
    */
-  function _burnFrom(address _account, uint _amount) internal {
+  function _burnFrom(address _account, uint _amount) internal virtual {
     _burn(_account, _amount);
     _approve(_account, msg.sender, allowances[_account][msg.sender].sub(_amount, "BEP2E: burn amount exceeds allowance"));
   }
+
+  /**
+  * @dev Hook that is called before any transfer of tokens. This includes
+  * minting and burning.
+  *
+  * Calling conditions:
+  *
+  * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+  * will be transferred to `to`.
+  * - when `from` is zero, `amount` tokens will be minted for `to`.
+  * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+  * - `from` and `to` are never both zero.
+  *
+  * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+  */
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual {}
+
+
+  /**
+  * @dev Hook that is called after any transfer of tokens. This includes
+  * minting and burning.
+  *
+  * Calling conditions:
+  *
+  * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+  * has been transferred to `to`.
+  * - when `from` is zero, `amount` tokens have been minted for `to`.
+  * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
+  * - `from` and `to` are never both zero.
+  *
+  * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+  */
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual {}  
 }
