d77b10ae4c6cee5594c5eb166a538a0b09837063
Ifeanyichukwu Welechi <welechi@icloud.com>
2022-02-06 08:59:47 +0100

    Update MockBNB.sol

--- contracts/mocks/MockBNB.sol
+++ contracts/mocks/MockBNB.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockETH is ERC20 {
+contract MockBNB is ERC20 {
     constructor() ERC20("Mock BNB", "BNB") {
         _mint(msg.sender, 1000000000000000000);
     }
