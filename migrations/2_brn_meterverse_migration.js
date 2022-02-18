2330bf9492f91c2212d4d3319e1980fdad6e516e
Dickens Odera <dickens@laevitas.ch>
2022-02-19 01:37:14 +0300

    update: minor update on brn migration file

--- migrations/2_brn_meterverse_migration.js
+++ migrations/2_brn_meterverse_migration.js
@@ -1,11 +1,15 @@
 const BrnMetaverse = artifacts.require('BrnMetaverse');
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
     const pancakeSwapRouterAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
     const marketingWalletAddress = '0x07871De755d9D922Cb1735A9F2418cBe9F1808EE';
     const liquidityFee = 600;
     const txFee = 200;
     const _lpBuyFee = 100;
     const _lpSellFee = 3000;
-    await deployer.deploy(BrnMetaverse, pancakeSwapRouterAddress, marketingWalletAddress,txFee, liquidityFee,_lpBuyFee,_lpSellFee);
+    if (network == 'bsc_testnet'){
+        await deployer.deploy(BrnMetaverse, pancakeSwapRouterAddress, accounts[0], txFee, liquidityFee, _lpBuyFee, _lpSellFee);
+    }else{
+        await deployer.deploy(BrnMetaverse, pancakeSwapRouterAddress, marketingWalletAddress, txFee, liquidityFee, _lpBuyFee, _lpSellFee);
+    }
 };
