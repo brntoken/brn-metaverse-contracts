5cecd01eddfe1e9d6fa78f6bd5275df5e6b20cfa
Dickens Odera <dickens@laevitas.ch>
2022-02-19 23:47:40 +0300

    feat: flatten contracts for bscscan verification

--- migrations/2_brn_meterverse_migration.js
+++ migrations/2_brn_meterverse_migration.js
@@ -3,10 +3,10 @@ const BrnMetaverse = artifacts.require('BrnMetaverse');
 module.exports = async function (deployer, network, accounts) {
     const pancakeSwapRouterAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
     const marketingWalletAddress = '0x07871De755d9D922Cb1735A9F2418cBe9F1808EE';
-    const liquidityFee = 600;
-    const txFee = 200;
-    const _lpBuyFee = 100;
-    const _lpSellFee = 3000;
+    const liquidityFee = 6;
+    const txFee = 5;
+    const _lpBuyFee = 5;
+    const _lpSellFee = 5;
     if (network == 'bsc_testnet'){
         await deployer.deploy(BrnMetaverse, pancakeSwapRouterAddress, accounts[0], txFee, liquidityFee, _lpBuyFee, _lpSellFee);
     }else{
